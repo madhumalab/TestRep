0d9f41374cc52e2cc20a1dbf94382b7f5e1882dd
madhumalab <madhumalab@cybage.com>
2014-10-07 11:02:17 +0530

    Added EF reference

---
+++ Rakuten.ShibuyaPJ/Models/GenericRepositiory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Rakuten.ShibuyaPJ.Models
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        internal ShibuyaDBEntities context;
+        internal DbSet<T> dbSet;
+
+        public GenericRepository()
+        {
+            this.context = new ShibuyaDBEntities();
+            dbSet = context.Set<T>();
+        }
+
+        public GenericRepository(ShibuyaDBEntities context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public T GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            dbSet.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            dbSet.Attach(obj);
+            context.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            T entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public void Delete(T obj)
+        {
+            if (context.Entry(obj).State == EntityState.Detached)
+            {
+                dbSet.Attach(obj);
+            }
+            dbSet.Remove(obj);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }       
+    }
+}
---
+++ Rakuten.ShibuyaPJ/Models/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakuten.ShibuyaPJ.Models
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Delete(T obj);
+        void Save();
+    }
+}
